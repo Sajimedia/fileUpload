721eaf71eac16c14b2c9ec0925575386b925af56
Sajith <sajimedia@gmail.com>
2018-09-12 12:45:09 +0530

    create a folder upload for storing images

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-var app = require('express')();
+var siofu = require("socketio-file-upload");
+var app = require('express')().use(siofu.router);
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+
 
 app.get('/', function(req, res) {
    res.sendfile('index.html');
 });
 io.on('connection', function(socket) {
    console.log('A user connected');
+   var uploader = new siofu();
+    uploader.dir = "uploads";
+    uploader.listen(socket);
 });
 
 http.listen(3000, function() {
